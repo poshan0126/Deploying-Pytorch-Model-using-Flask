15333e69a5c8a66ea0e4ddeb0caf95a4a0c28f7f
Nischal Lal Shrestha <aakrist666@gmail.com>
2019-02-10 01:18:32 +0545

    Adds methods to upload the file

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/check/', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('upload_file.html')
+    if request.method == 'POST':
+        predicted_flower = 'Lily'
+        return render_template('result_file.html', flower = predicted_flower)
 
 if __name__ == "__main__":
     app.run()
